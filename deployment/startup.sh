5f8f2250f23856c6d7039ba17fae96a1573a25a6
Tien Nguyen <luantien411@gmail.com>
2022-07-18 15:14:57 +0700

    Hotfix startup script: correct default nginx config path

--- deployment/startup.sh
+++ deployment/startup.sh
@@ -1,4 +1,4 @@
 php artisan optimize:clear
 php artisan migrate --force
-cp /home/site/default /etc/nginx/sites-available/default
+cp /home/site/wwwroot/deployment/nginx/default /etc/nginx/sites-available/default
 service nginx reload
